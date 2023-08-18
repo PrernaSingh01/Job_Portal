fb911bc69cbb3311f5b55d5da7781acf5b391eda
Prerna <iprernasingh4@gmail.com>
2023-08-18 16:32:44 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 //imports
 import express from "express";
+import dotenv from "dotenv";
+
+//Dot ENV config
+dotenv.config();
 
 //rest object
 const app = express();
@@ -8,6 +12,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1> Hello <h1>");
 });
+
+//port
+Const PORT = process.env.PORT || 3000;
 
 //listen
 app.listen(3000, () => {
