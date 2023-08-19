acae67dc32f2a4b53585fea92d88b99f7617770d
Prerna <iprernasingh4@gmail.com>
2023-08-19 17:50:04 +0530

    first commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,32 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 //schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is require"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is require"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      require: [true, "Email is require"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      require: [true, "Password is require"],
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
   },
-  email: {
-    type: String,
-    require: [true, "Email is require"],
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
