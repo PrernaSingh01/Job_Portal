bad5b7dce2bdb470b72c804057a8d9bd26705b4d
Prerna <iprernasingh4@gmail.com>
2023-08-20 17:41:58 +0530

    first commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,45 +1,36 @@
 import userModel from "../models/userModel.js";
 
 export const registerController = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    //validate
-    if (!name) {
-      return res
-        .status(400)
-        .send({ success: false, message: "please provide name" });
-    }
-    if (!email) {
-      return res
-        .status(400)
-        .send({ success: false, message: "please provide email" });
-    }
-    if (!password) {
-      return res
-        .status(400)
-        .send({ success: false, message: "please provide password" });
-    }
+  const { name, email, password } = req.body;
+  //validate
+  if (!name) {
+    return res
+      .status(400)
+      .send({ success: false, message: "please provide name" });
+  }
+  if (!email) {
+    return res
+      .status(400)
+      .send({ success: false, message: "please provide email" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .send({ success: false, message: "please provide password" });
+  }
 
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      return res.status(200).send({
-        success: false,
-        message: "Email Already Registere Please Login",
-      });
-    }
-
-    const user = await userModel.create({ name, email, password });
-    res.status(201).send({
-      success: true,
-      message: "User Created Succesfully",
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({
-      message: "Error In Register Controller",
+  const existingUser = await userModel.findOne({ email });
+  if (existingUser) {
+    return res.status(200).send({
       success: false,
-      error,
+      message: "Email Already Registere Please Login",
     });
   }
+
+  const user = await userModel.create({ name, email, password });
+  res.status(201).send({
+    success: true,
+    message: "User Created Succesfully",
+    user,
+  });
 };
