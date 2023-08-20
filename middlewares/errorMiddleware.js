5bc30ec4649052b5d8ab74369d3db696d5ad9b34
Prerna <iprernasingh4@gmail.com>
2023-08-20 18:01:57 +0530

    first commit

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -17,6 +17,9 @@ const errorMiddleware = (err, req, res, next) => {
   //duplicate error
   if (err.code && err.code === 11000) {
     defaultErrors.statusCode = 400;
+    defaultErrors.message = `${Object.keys(
+      err.keyValue
+    )} field has to be unique`;
   }
   res.status(defaultErrors.statusCode).json({ message: defaultErrors.message });
 };
