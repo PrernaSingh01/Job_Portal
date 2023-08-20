a18d2d9a6644605d5b15274f25f3ab50ff440c1e
Prerna <iprernasingh4@gmail.com>
2023-08-20 17:59:22 +0530

    first commit

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,5 +2,20 @@
 
 const errorMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500);
+  const defaultErrors = {
+    statusCode: 500,
+    message: err,
+  };
+
+  //missing field error
+  if (err.name === "ValidationError") {
+    defaultErrors.statusCode = 400;
+    defaultErrors.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  //duplicate error
+  res.status(defaultErrors.statusCode).json({ message: defaultErrors.message });
 };
+
+export default errorMiddleware;
