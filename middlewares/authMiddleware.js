17041f0e8abcb827f1f7081bfd17ba0b034f29fe
Prerna <iprernasingh4@gmail.com>
2023-08-22 16:37:51 +0530

    first commit

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,16 @@
 import JWT from "jsonwebtoken";
 
-const userAuth = async (req, res, next) => {};
+const userAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorixation;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    next("Auth Failed");
+  }
+  const token = authHeader.splite(" ")[1];
+  try {
+    const payload = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = { userid: payload.userId };
+    next();
+  } catch (error) {
+    next("Auth Failed");
+  }
+};
