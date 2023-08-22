d47fed2906ce7b92fcab917a9d12530fb2bad3c4
Prerna <iprernasingh4@gmail.com>
2023-08-22 19:18:20 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,29 @@
-export const updateUserController = async (req, res, next) => {
-  const { name, email, lastName, location } = req.body;
-  if (!name || !email || !lastName || !password) {
-    next("please provide all fields");
-  }
-  const user = await userModel.findOne({ _id: req.user.userId });
-  user.name = name;
-  user.lastName = lastName;
-  user.email = emailuser.location;
+import userModel from "../models/userModel.js";
 
-  await user.save();
-  const token = user.createJWT();
-  res.status(200).json({
-    user,
-    token,
-  });
+export const updateUserController = async (req, res, next) => {
+  const { name, email, lastName, password, location } = req.body;
+  if (!name || !email || !lastName || !password) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
+  try {
+    const user = await userModel.findOne({ _id: req.user.userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.lastName = lastName;
+    user.email = email;
+    user.location = location;
+
+    await user.save();
+    const token = user.createJWT();
+    return res.status(200).json({
+      user,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
