adf669c49c69684d6e6d0e874cbc759b2bf5c41b
Prerna <iprernasingh4@gmail.com>
2023-08-27 19:06:33 +0530

    first commit

--- models/jobsModel.js
+++ models/jobsModel.js
@@ -1,24 +1,36 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: [true, "Company name is require"],
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Company name is require"],
+    },
+    position: {
+      type: String,
+      required: [true, "Job position is required"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "reject", "interview"],
+      default: "pending",
+    },
+    workType: {
+      type: String,
+      enum: ["full-time", "part-time", "internship", "contaract"],
+      default: "full-time",
+    },
+    workLocation: {
+      type: String,
+      default: "Mumbai",
+      required: [true, "Work location is required"],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
   },
-  position: {
-    type: String,
-    required: [true, "Job position is required"],
-  },
-  status: {
-    type: String,
-    enum: ["pending", "reject", "interview"],
-    default: "pending",
-  },
-  workType: {
-    type: String,
-    enum: ["full-time", "part-time", "internship", "contaract"],
-    default: "full-time",
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("job", jobSchema);
