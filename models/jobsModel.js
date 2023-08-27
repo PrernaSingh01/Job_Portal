843351be786aac3c4800d4271acb28de19bc1fb2
Prerna <iprernasingh4@gmail.com>
2023-08-27 18:49:08 +0530

    first commit

--- models/jobsModel.js
+++ models/jobsModel.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({});
+const jobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Company name is require"],
+  },
+  position:{
+    type:String,
+    required:[true, 'Job position is required'],
+  },{
+
+  }
+});
 
 export default mongoose.model("job", jobSchema);
