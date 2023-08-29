3da64fa7b122a31358dec6a348f2d927c40c6230
Prerna <iprernasingh4@gmail.com>
2023-08-29 18:15:19 +0530

    first commit

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -23,4 +23,26 @@ export const getAllJobsController = async (req, res, next) => {
 //=======UPDATE JOBS ========
 export const updateJobController = async (req, res, next) => {
   const { id } = req.params;
+  const { company, position } = req.body;
+
+  //validaton
+  if (!company || !position) {
+    next("Please Provide All Fields");
+  }
+
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+
+  //validation
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
+  if (req.user.userId === job.createdBy.toString()) {
+    return;
+    next("Your Not Authorized to update this job");
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 };
